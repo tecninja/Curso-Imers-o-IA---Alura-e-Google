65696193aaaac73797b536c0dee62806e5332e1c
esdras-santos-rethink <contato.esdrassantos@gmail.com>
2024-05-10 18:29:33 -0300

    criação de chamadas ao s3

--- backend/engine.py
+++ backend/engine.py
@@ -4,9 +4,9 @@ import boto3
 import os
 
 API_KEY = st.secrets['gemini_token']
-os.environ['Access key ID'] = st.secrets['Access_key_ID']
-os.environ['Secret access key'] = st.secrets['Secret_access_key']
-os.environ['region'] = st.secrets['region']
+os.environ['Access key ID'] = st.secrets['AWS_ACCESS_KEY_ID']
+os.environ['Secret access key'] = st.secrets['AWS_SECRET_ACCESS_KEY']
+os.environ['region'] = st.secrets['AWS_DEFAULT_REGION']
 
 
 class Ia:
@@ -17,17 +17,42 @@ class Ia:
     
 class Nuvem:
     
+    BUCKET = "exms-treino-primeiro-bucket"
     
     def __init__(self) -> None:
         self.s3 = boto3.resource("s3")
     
 
     def enviar_objeto(self, obj, obj_name):
+        
         try:
-            self.s3.Bucket("exms-treino-primeiro-bucket").put_object(Key=obj_name, Body=obj)
+            self.s3.Bucket(self.BUCKET).put_object(Key=obj_name, Body=obj)
         except Exception as e:
-            return {"status":False, "contexto": f"Erro no envio: {e}"}
+            return {"status":False,
+                    "contexto": f"Erro no envio do arquivo {obj_name}: {e}"}
         else:
-            return {"status":True, "contexto": "Arquivo enviado com sucesso"}
-            
-            
+            return {"status":True,
+                    "contexto": f"Arquivo {obj_name} enviado com sucesso"}
+        
+    def listar_objetos(self):
+        try:
+            bkt = self.s3.Bucket(self.BUCKET)
+            objetos = [obj.key for obj in bkt.objects.all()]
+        except Exception as e:
+            return {"status":False,
+                    "contexto":f"Erro na busca - {e}"}
+        else:
+            return {"status":True,
+                    "contexto":"Objetos listados com sucesso",
+                    "objetos":objetos}
+    
+    def apagar_objetos(self, obj_name):
+        try:
+            self.s3.Object(self.BUCKET, obj_name).delete()
+        except Exception as e:
+            return {"status":False,
+                    "contexto": f"Erro ao apagar arquivo {obj_name}. Detalhamento: {e}"}
+        else:
+            return {"status":True,
+                    "contexto": f"Arquivo {obj_name} apagado com sucesso"
+                    }
--- pages/Cadastro_de_Contexto.py
+++ pages/Cadastro_de_Contexto.py
@@ -8,20 +8,40 @@ class Cadastro:
         
         
         with st.expander("Cadastro"):
-            file = st.file_uploader("Selecione o arquivo", accept_multiple_files=True)
+            file = st.file_uploader(
+                "Selecione o arquivo",
+                accept_multiple_files=True
+                )
             
             if st.button("Enviar Arquivo"):
                 if file:
                     for item in file:
-                        resposta = Nuvem().enviar_objeto(obj=item.getvalue(), obj_name=item.name)
+                        resposta = Nuvem().enviar_objeto(
+                            obj=item.getvalue(),
+                            obj_name=item.name
+                            )
                         if resposta["status"]:
-                            st.success(resposta["contexto"])
+                            st.success(resposta['contexto'])
                         else:
-                            st.error(resposta["contexto"])
+                            st.error(resposta['contexto'])
                 else:
                     st.error("Nenhum arquivo selecionado")
                 
         with st.expander("Exclusão"):
-            st.empty()
+            resposta = Nuvem().listar_objetos()
+            if resposta['status']:
+                selecao = st.radio(
+                    label="Escolha qual arquivo excluir",
+                    options=resposta['objetos']
+                )
+                
+                if st.button("Apagar Arquivo"):
+                    resposta = Nuvem().apagar_objetos(selecao)
+                    if resposta['status']:
+                        st.success(resposta['contexto'])
+                    else:
+                        st.error(resposta['contexto'])    
+            else:
+                st.error(f"Erro ao buscar objetos: {resposta['contexto']}")
 
 Cadastro()
